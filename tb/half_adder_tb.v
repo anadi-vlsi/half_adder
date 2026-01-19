36449c1acf4eb2bfaf1ed77ca5d85273e8b8913c
Anadi Chauhan <anadichauhan454@gmail.com>
2026-01-19 21:15:58 +0530

    Create half_adder_tb.v

---
+++ tb/half_adder_tb.v
@@ -0,0 +1,35 @@
+module half_adder_tb;
+
+reg x;
+reg y;
+wire z;
+wire a;
+
+half_adder HA (.a(x),
+		.b(y),
+		 .sum(z),
+		  .carry(a)
+);
+
+initial 
+begin
+
+	x=0;
+	y=0;
+	 #10;
+
+	x=0;
+	y=1;
+	 #20;
+	
+	x=1;
+	y=0;
+	 #20;
+
+	x=1;
+	y=1;
+	 #20;
+
+$finish;
+end
+endmodule
